5b63397031de092a4dffc08e1c0d1e96f596c0a9
Rushikesh Dalvi <dalvi.ru@northeastern.edu>
2022-10-05 15:16:05 -0400

    removed swagger code

--- webapp/server.js
+++ webapp/server.js
@@ -5,32 +5,8 @@ const app = express(),
       fs = require('fs'),
       port = 3000;
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-// place holder for the data
-let tasks = [
-  {
-    id: 1,
-    task: 'task1',
-    assignee: 'dhaval',
-  },
-  {
-    id: 2,
-    task: 'task2',
-    assignee: 'aishwarya',
-  },
-  {
-    id: 3,
-    task: 'task3',
-    assignee: 'tejas',
-  },
-  
-];
 
 app.use(bodyParser.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.get('/api/healthz', (req, res) => {
   console.log('api/healthcheck called!!')
   res.json(tasks);
