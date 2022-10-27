daa4d9e9e83ccd9f5ee3d755ce55d736a3113e14
Rushikesh Dalvi <dalvi.ru@northeastern.edu>
2022-10-27 12:07:24 -0400

    testing zip

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require("dotenv").config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -10,11 +10,12 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(
+  config.database,
+  config.user,
+  config.password,
+  config
+)
 
 fs
   .readdirSync(__dirname)
