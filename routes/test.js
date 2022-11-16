e1f6f0cbba62407b072c149858ea434407d86067
Rushikesh Dalvi <dalvi.ru@northeastern.edu>
2022-11-15 23:05:49 -0500

    sns

--- routes/test.js
+++ routes/test.js
@@ -146,9 +146,7 @@ con.get("/v1/account/:id", async (req, res) => {
 
 con.post("/v1/account", createUser);
 
-
-
-
+// Create a User
 async function createUser(req, res, next) {
   console.log('create userrr')
   var hash = await bcrypt.hash(req.body.password, 10);
@@ -171,27 +169,7 @@ async function createUser(req, res, next) {
   });
 
   console.log('verified and existing 1');
-  
-  async function getUser(req, res, next) {
-    const user = await getUserByUsername(req.user.username);
-    if (user) {
-        logger.info("get user 200");
-        res.status(200).send({
-            id: user.dataValues.id,
-            first_name: user.dataValues.first_name,
-            last_name: user.dataValues.last_name,
-            username: user.dataValues.username,
-            account_created: user.dataValues.createdAt,
-            account_updated: user.dataValues.updatedAt,
-            isVerified: user.dataValues.isVerified
-        });
-    } else {
-        res.status(400).send({
-            message: 'User not found!'
-        });
-    }
-  }
-  
+
  
   if (getUser) {
       console.log('verified and existing', getUser.dataValues.isVerified);
@@ -278,15 +256,165 @@ async function createUser(req, res, next) {
   }
 }
 
+// Verify user
+async function verifyUser(req, res, next) {
+  console.log('verifyUser :');
+  console.log('verifyUser :', req.query.email);
+  const user = await getUserByUsername(req.query.email);
+  if (user) {
+      console.log('got user  :');
+      if (user.dataValues.isVerified) {
+          res.status(202).send({
+              message: 'Already Successfully Verified!'
+          });
+      } else {
 
+          var params = {
+              TableName: 'csye-6225',
+              Key: {
+                  'Email': {
+                      S: req.query.email
+                  },
+                  'TokenName': {
+                      S: req.query.token
+                  }
+              }
+          };
+          console.log('got user  param:');
+          // Call DynamoDB to read the item from the table
 
+          dynamoDatabase.getItem(params, function (err, data) {
+              if (err) {
+                  console.log("Error", err);
+                  res.status(400).send({
+                      message: 'unable to verify'
+                  });
+              } else {
+                  console.log("Success dynamoDatabase getItem", data.Item);
+                  try {
+                      var ttl = data.Item.TimeToLive.N;
+                      var curr = new Date().getTime();
+                      console.log(ttl);
+                      console.log('time diffrence', curr - ttl);
+                      var time = (curr - ttl) / 60000;
+                      console.log('time diffrence ', time);
+                      if (time < 5) {
+                          if (data.Item.Email.S == user.dataValues.username) {
+                              User.update({
+                                  isVerified: true,
+                              }, {
+                                  where: {
+                                      username: req.query.email
+                                  }
+                              }).then((result) => {
+                                  if (result == 1) {
+                                      logger.info("update user 204");
+                                      sdc.increment('endpoint.userUpdate');
+                                      res.status(200).send({
+                                          message: 'Successfully Verified!'
+                                      });
+                                  } else {
+                                      res.status(400).send({
+                                          message: 'unable to verify'
+                                      });
+                                  }
+                              }).catch(err => {
+                                  res.status(500).send({
+                                      message: 'Error Updating the user'
+                                  });
+                              });
+                          } else {
+                              res.status(400).send({
+                                  message: 'Token and email did not matched'
+                              });
+                          }
+                      } else {
+                          res.status(400).send({
+                              message: 'token Expired! Cannot verify Email'
+                          });
+                      }
+                  } catch (err) {
+                      console.log("Error", err);
+                      res.status(400).send({
+                          message: 'unable to verify'
+                      });
+                  }
+              }
+          });
 
+      }
+  } else {
+      res.status(400).send({
+          message: 'User not found!'
+      });
+  }
+}
 
+//Get a User
+async function getUser(req, res, next) {
+  const user = await getUserByUsername(req.user.username);
+  if (user) {
+      logger.info("get user 200");
+      res.status(200).send({
+          id: user.dataValues.id,
+          first_name: user.dataValues.first_name,
+          last_name: user.dataValues.last_name,
+          username: user.dataValues.username,
+          account_created: user.dataValues.createdAt,
+          account_updated: user.dataValues.updatedAt,
+          isVerified: user.dataValues.isVerified
+      });
+  } else {
+      res.status(400).send({
+          message: 'User not found!'
+      });
+  }
+}
 
+// Update a user
 
+async function updateUser(req, res, next) {
+  if (req.body.username != req.user.username) {
+      logger.error("can not update user 400");
+      res.status(400);
+  }
+  if (!req.body.first_name || !req.body.last_name || !req.body.username || !req.body.password) {
+      logger.error("/update user failed 400");
+      res.status(400).send({
+          message: 'Enter all parameters!'
+      });
+  }
+  User.update({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      password: await bcrypt.hash(req.body.password, 10)
+  }, {
+      where: {
+          username: req.user.username
+      }
+  }).then((result) => {
+      if (result == 1) {
+          logger.info("update user 204");
+          sdc.increment('endpoint.userUpdate');
+          res.sendStatus(204);
+      } else {
+          res.sendStatus(400);
+      }
+  }).catch(err => {
+      res.status(500).send({
+          message: 'Error Updating the user'
+      });
+  });
+}
 
+async function getUserByUsername(username) {
 
-
+  return User.findOne({
+      where: {
+          username: username
+      }
+  });
+}
 
 
 
